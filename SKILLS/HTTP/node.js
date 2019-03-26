8b7424e511b37f41a92766a4a6b2e2873e4f6c1b
AGoblinKing <AGoblinKing@isekai.dev>
2019-03-25 21:08:54 -0700

    Removed SSL and github webhook support for now

--- SKILLS/HTTP/node.js
+++ SKILLS/HTTP/node.js
@@ -1,6 +1,4 @@
 import express from "express";
-import greenlock from "greenlock-express";
-import http from "http";
 import helmet from "helmet";
 
 import sub_skills from "./SKILLS/*.js";
@@ -13,7 +11,6 @@ export default ({
 }) => EQUIP({
     HTTP: ({
         HTTP: {
-            SSL = false,
             port = 8080
         },
         SET,
@@ -21,77 +18,11 @@ export default ({
         const app = express();
         app.use(helmet());
 
-        if(SSL) {
-            const {
-                agreeTos,
-                email,
-                approvedDomains,
-                configDir = `~/.config/ssl`,
-                communityMember = false,
-                telemetry = false,
-                ssl_port = 443,
-                server = `staging`,
-                redirect = 80
-            } = SSL;
-        
-            if(!agreeTos || !email || !approvedDomains) {
-                const not_filled = Object.entries({ 
-                    agreeTos,
-                    email,
-                    approvedDomains 
-                }).
-                    filter(([ , value ]) => value).
-                    map((key) => key).
-                    join(` - `);
-        
-                throw new Error(`You've enabled SSL but not configured SSL: ${not_filled}`);
-            }
-            
-            greenlock.create({
-                telemetry,
-                agreeTos,
-                email,
-                configDir,
-                server,
-                communityMember,
-                approvedDomains,
-                app,
-            }).
-                listen(redirect, ssl_port);
-        
+        app.listen(port, () => {
+            console.log(`[HTTP]`);
+            console.log(`Listening on http://localhost:8080`);
+        });
 
-            console.log(`[HTTPS]`);
-            console.log(`BIFROST SECURED`);
-                
-            /*
-             * if(redirect) {
-             *     const redirectApp = express();
-             */
-            
-            //     console.log(`Redirecting to HTTP to HTTPS`);
-        
-            /*
-             *     redirectApp.all(`*`, (req, res) => {  
-             *         res.redirect(`https://${req.headers.host}${req.url}`);
-             *         res.end();
-             *     });
-             */
-        
-            /*
-             *     http.createServer(redirectApp).
-             *         listen(redirect);
-             */
-        
-            // }
-        
-        } else {
-            app.listen(port, () => {
-                console.log(`⚠️[HTTP]⚠️`);
-                console.log(`BIFROST INSECURE`);
-                console.log(`Listening on http://localhost:8080`);
-            });
-        }
-            
         SET({
             HTTP: app
         });
